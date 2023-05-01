b36cfa78c06e3b08b9980df7af4aeb641c160d3c
Luis Cordova <ageofculturebiz@gmail.com>
2023-05-01 18:03:05 -0400

    cors bug

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log('FRONTEND_URI:', process.env.FRONTEND_URI);
+
 app.use(
     cors({
       origin: [process.env.FRONTEND_URI]  // <== URL of our future React app
